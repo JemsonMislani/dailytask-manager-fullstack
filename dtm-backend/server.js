89c6345d1609e4f10531df666e3abb2d9477386b
Jemson <jemsonmislani@gmail.com>
2026-06-09 10:57:24 +0800

    set up postgres.

--- dtm-backend/server.js
+++ dtm-backend/server.js
@@ -9,6 +9,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const pool = new Pool({
+    user: 'postgres',
+    password: 'Im_Jem23*',
+    host: 'localhost',
+    database: 'dailytask_manager_fullstack',
+    port: 5432
+})
+
+
 const PORT = 3004;
 app.listen(PORT, () => {
     console.log(`Jem! your server is running on port ${PORT}`)
