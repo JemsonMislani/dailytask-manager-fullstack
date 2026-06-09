0b2416a785ed8777f7bf81345e4c62bee458d375
Jemson <jemsonmislani@gmail.com>
2026-06-09 12:11:14 +0800

    Create dashboard component. Create Login form, Validate it, Make it interactive. Do routing.

--- dtm-frontend/src/App.jsx
+++ dtm-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import CreateAcc from './CreateAcc'
 import LoginAcc from './LoginAcc'
+import Dashboard from './Dashboard'
 
 export default function App() {
 
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<LoginAcc />}/>
         <Route path='/create' element={<CreateAcc />}/>
+        <Route path='/success' element={<Dashboard />}/>
       </Routes>
       </BrowserRouter>
     </>
---
+++ dtm-frontend/src/Dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+
+  return (
+    <>
+        <h1>Successfully login</h1>
+    </>
+  )
+}
+
